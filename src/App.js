4f3cc1bc2fe5fecb546fa206ca17f9fa1fac4b7f
Kris Kristofferson Nabua <kc.nabua@goabroad.com>
2018-12-05 17:56:36 +0800

    updated title and icon

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import logo from './logo.svg'
 import Header from './components/header/'
 import Footer from './components/footer'
 import Skills from './components/skills'
+import { Helmet } from 'react-helmet'
 import './App.css'
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
+				<Helmet>
+					<title>KC | CV</title>
+					<link
+						rel="shortcut icon"
+						href={require('./assets/favicon.ico')}
+					/>
+				</Helmet>
 				<Header />
 				<img
 					className="img-background"
